9025da6d513cabe745800d74136561b41df24f23
Zettsuen <61870005+Zettsuen@users.noreply.github.com>
2020-12-03 16:11:51 +0100

    App 1

---
+++ App1/GrabadoraNexus/src/Captura.FFmpeg/ArgsBuilder/FFmpegArgs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Captura.Models
+{
+    public abstract class FFmpegArgs
+    {
+        public readonly List<string> Args = new List<string>();
+
+        public virtual string GetArgs()
+        {
+            return string.Join(" ", Args);
+        }
+    }
+}
