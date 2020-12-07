3015977e9e41df2321ad7d4e0a9702389b83e9e1
Zettsuen <61870005+Zettsuen@users.noreply.github.com>
2020-12-07 16:04:17 +0100

    Seguimiento clips

--- JISUS_2/quick-screen-recorder-master/quick-screen-recorder/MainForm.cs
+++ JISUS_2/quick-screen-recorder-master/quick-screen-recorder/MainForm.cs
@@ -9,10 +9,20 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Runtime.InteropServices;
 using QuickLibrary;
+using System.Text;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Permissions;
+using System.Diagnostics;
 
 namespace quick_screen_recorder
 {
-	public partial class MainForm : Form
+
+
+public partial class MainForm : Form
 	{
 		private AreaForm areaForm;
 		private StopForm stopForm;
@@ -46,6 +56,94 @@ namespace quick_screen_recorder
 			applyDarkTheme(darkMode);
 		}
 
+		private static void Run(string args)
+		{
+
+			// If a directory is not specified, exit program.
+			/*if (args.Length != 2)
+			{
+				// Display the proper way to call the program.
+				Console.WriteLine("Usage: Watcher.exe (directory)");
+				return;
+			}*/
+
+			// Create a new FileSystemWatcher and set its properties.
+			using (FileSystemWatcher watcher = new FileSystemWatcher())
+			{
+				watcher.Path = args;
+
+				// Watch for changes in LastAccess and LastWrite times, and
+				// the renaming of files or directories.
+				watcher.NotifyFilter = NotifyFilters.LastAccess
+									 | NotifyFilters.LastWrite
+									 | NotifyFilters.FileName
+									 | NotifyFilters.DirectoryName;
+
+				// Only watch text files.
+				watcher.Filter = "*.mp4";
+
+				// Add event handlers.
+				watcher.Created += OnChanged;
+
+				// Begin watching.
+				watcher.EnableRaisingEvents = true;
+
+				// Wait for the user to quit the program.
+				Console.WriteLine("Press 'q' to quit the sample.");
+				while (Console.Read() != 'q') ;
+			}
+		}
+
+		// Define the event handlers.
+		private static void OnChanged(object source, FileSystemEventArgs e)
+		{
+			String path = Path.GetFileName(e.FullPath);
+			String directory = Path.GetDirectoryName(e.FullPath);
+			int position = e.FullPath.IndexOf(path);
+			Console.WriteLine(directory.Length);
+			String new_directory = e.FullPath.Substring(0, directory.Length - 7);
+			new_directory += "NexusClips\\";
+			Console.WriteLine(new_directory);
+			try
+			{
+				// Determine whether the directory exists.
+				if (Directory.Exists(new_directory))
+				{
+					Console.WriteLine("That path exists already.");
+
+				}
+				else
+				{
+
+					// Try to create the directory.
+					DirectoryInfo di = Directory.CreateDirectory(new_directory);
+					Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(new_directory));
+
+				}
+
+
+			}
+			catch (Exception a)
+			{
+				Console.WriteLine("The process failed: {0}", a.ToString());
+			}
+			finally { }
+			Process cmd = new Process();
+			cmd.StartInfo.FileName = "cmd.exe";
+			cmd.StartInfo.RedirectStandardInput = true;
+			cmd.StartInfo.RedirectStandardOutput = true;
+			cmd.StartInfo.CreateNoWindow = true;
+			cmd.StartInfo.UseShellExecute = false;
+			cmd.Start();
+			cmd.StandardInput.WriteLine($"ffmpeg.exe -sseof -30 -i {e.FullPath} -vcodec libx264 -crf 28 {new_directory}jugada-nexus-{path}");
+			cmd.StandardInput.Flush();
+			cmd.StandardInput.Close();
+			cmd.WaitForExit();
+			Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+			File.Delete(e.FullPath);
+
+
+		}
 		private void applyDarkTheme(bool darkMode)
 		{
 			if (darkMode)
@@ -202,15 +300,61 @@ namespace quick_screen_recorder
 				MessageBox.Show("Something went wrong!", "Error");
 			}
 		}
+		// Instantiate random number generator.  
+		private readonly Random _random = new Random();
+
+		// Generates a random number within a range.      
+		public int RandomNumber(int min, int max)
+		{
+			return _random.Next(min, max);
+		}
+		// Generates a random string with a given size.    
+		public string RandomString(int size, bool lowerCase = false)
+		{
+			var builder = new StringBuilder(size);
+
+			// Unicode/ASCII Letters are divided into two blocks
+			// (Letters 65–90 / 97–122):
+			// The first group containing the uppercase letters and
+			// the second group containing the lowercase.  
+
+			// char is a single Unicode character  
+			char offset = lowerCase ? 'a' : 'A';
+			const int lettersOffset = 26; // A...Z or a..z: length=26  
+
+			for (var i = 0; i < size; i++)
+			{
+				var @char = (char)_random.Next(offset, offset + lettersOffset);
+				builder.Append(@char);
+			}
+
+			return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+		}
+		// Generates a random password.  
+		// 4-LowerCase + 4-Digits + 2-UpperCase  
+		public string RandomPassword()
+		{
+			var passwordBuilder = new StringBuilder();
+
+			// 4-Letters lower case   
+			passwordBuilder.Append(RandomString(4, true));
+
+			// 4-Digits between 1000 and 9999  
+			passwordBuilder.Append(RandomNumber(1000, 9999));
+
+			// 2-Letters upper case  
+			passwordBuilder.Append(RandomString(2));
+			return passwordBuilder.ToString();
+		}
 
 		private void CheckStartRec()
 		{
-			string path = folderTextBox.Text + "/" + fileNameTextBox.Text + ".avi";
+			string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Nexus/" + RandomPassword() + ".avi");
 
 			if (File.Exists(path))
 			{
 				DialogResult window = MessageBox.Show(
-					fileNameTextBox.Text + ".avi ya existe.\n¿Quieres reemplazar y empezar a grabar?",
+					path + ".avi ya existe.\n¿Quieres reemplazar y empezar a grabar?",
 					"Atención",
 					MessageBoxButtons.YesNo,
 					MessageBoxIcon.Question
@@ -219,11 +363,13 @@ namespace quick_screen_recorder
 				if (window == DialogResult.Yes)
 				{
 					StartRec(path);
+					Run(path);
 				}
 			}
 			else
 			{
 				StartRec(path);
+				Run(path);
 			}
 		}
 
@@ -270,6 +416,7 @@ namespace quick_screen_recorder
 				stopForm = new StopForm(DateTime.Now, darkMode, videoStr, audioStr);
 				stopForm.Owner = this;
 				stopForm.Show();
+
 			}
 			catch
 			{
