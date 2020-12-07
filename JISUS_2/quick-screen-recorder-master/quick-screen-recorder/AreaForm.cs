41e517d4d43082f1dbf61fb4769f73ad02bdf47e
RoactionMan <64604673+RoactionMan@users.noreply.github.com>
2020-12-07 14:06:18 +0100

    SUPER APP

---
+++ JISUS_2/quick-screen-recorder-master/quick-screen-recorder/AreaForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace quick_screen_recorder
+{
+	public partial class AreaForm : Form
+	{
+		private const int WM_NCLBUTTONDOWN = 0xA1;
+		private const int HT_CAPTION = 0x2;
+
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern bool ReleaseCapture();
+
+		private Point startPos;
+		private Size curSize;
+
+		private System.Timers.Timer resizeTimer = new System.Timers.Timer();
+
+		private int maxWidth = 4096;
+		private int maxHeight = 4096;
+
+		public AreaForm()
+		{
+			InitializeComponent();
+
+			resizeTimer.Elapsed += new ElapsedEventHandler(resizeTimer_Elapsed);
+			resizeTimer.Interval = 50;
+		}
+
+		private void resizeTimer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			this.Invoke((MethodInvoker)(() => {
+				Point curPos = this.PointToClient(Cursor.Position);
+
+				int newWidth = curSize.Width + curPos.X - startPos.X;
+				int newHeight = curSize.Height + curPos.Y - startPos.Y;
+
+				if (newWidth > 4096) newWidth = 4096;
+				if (newHeight > 4096) newHeight = 4096;
+
+				(this.Owner as MainForm).SetAreaWidth(newWidth);
+				(this.Owner as MainForm).SetAreaHeight(newHeight);
+			}));
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+		}
+
+		private void AreaForm_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				Point downPos = this.PointToClient(Cursor.Position);
+				if (downPos.X > dragBtn.Location.X && downPos.X < dragBtn.Location.X + dragBtn.Width &&
+					downPos.Y > dragBtn.Location.Y && downPos.Y < dragBtn.Location.Y + dragBtn.Height)
+				{
+					Cursor.Current = Cursors.SizeNWSE;
+					startPos = downPos;
+					curSize = this.Size;
+					resizeTimer.Start();
+				}
+				else
+				{
+					Cursor.Current = Cursors.SizeAll;
+					ReleaseCapture();
+					SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+				}
+			}
+		}
+
+		private void AreaForm_MouseUp(object sender, MouseEventArgs e)
+		{
+			resizeTimer.Stop();
+			Cursor.Current = Cursors.Default;
+		}
+
+		private void AreaForm_SizeChanged(object sender, EventArgs e)
+		{
+			this.Refresh();
+			(this.Owner as MainForm).SetMaximumX(maxWidth - this.Width);
+			(this.Owner as MainForm).SetMaximumY(maxHeight - this.Height);
+		}
+
+		private void AreaForm_LocationChanged(object sender, EventArgs e)
+		{
+			(this.Owner as MainForm).SetAreaX(this.Left);
+			(this.Owner as MainForm).SetAreaY(this.Top);
+		}
+
+		public void SetMaximumArea(int maxW, int maxH)
+		{
+			maxWidth = maxW;
+			maxHeight = maxH;
+		}
+
+		private void AreaForm_ResizeEnd(object sender, EventArgs e)
+		{
+			if (this.Left < 0) this.Left = 0;
+			if (this.Top < 0) this.Top = 0;
+
+			if (this.Left + this.Width > maxWidth) this.Left = maxWidth - this.Width;
+			if (this.Top + this.Height > maxHeight) this.Top = maxHeight - this.Height;
+		}
+	}
+}
---
+++ JISUS_2/quick-screen-recorder-master/quick-screen-recorder/NotiForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace quick_screen_recorder
+{
+    partial class NotiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotiForm));
+            this.notificacio = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerTancarNoti = new System.Windows.Forms.Timer(this.components);
+            this.FadeIn = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notificacio
+            // 
+            this.notificacio.AutoSize = true;
+            this.notificacio.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notificacio.ForeColor = System.Drawing.Color.White;
+            this.notificacio.Location = new System.Drawing.Point(25, 76);
+            this.notificacio.Name = "notificacio";
+            this.notificacio.Size = new System.Drawing.Size(46, 17);
+            this.notificacio.TabIndex = 0;
+            this.notificacio.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(110, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timerTancarNoti
+            // 
+            this.timerTancarNoti.Interval = 4000;
+            this.timerTancarNoti.Tick += new System.EventHandler(this.timerTancarNoti_Tick_1);
+            // 
+            // FadeIn
+            // 
+            this.FadeIn.Interval = 30;
+            // 
+            // NotiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 118);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.notificacio);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotiForm";
+            this.Text = "NotiForm";
+            this.Load += new System.EventHandler(this.NotiForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label notificacio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerTancarNoti;
+        private System.Windows.Forms.Timer FadeIn;
+    }
+}
